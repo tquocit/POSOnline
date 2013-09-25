316c8d28c7967087d8639a887390ce2dc78e3758
Quoc Nguyen <trongquoc.nguyen@2359media.com>
2013-09-25 14:12:24 +0700

    Create new project

---
+++ POSOnline/POSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  POSAppDelegate.h
+//  POSOnline
+//
+//  Created by Quoc Nguyen on 9/25/13.
+//  Copyright (c) 2013 Quoc Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface POSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ POSOnline/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  POSOnline
+//
+//  Created by Quoc Nguyen on 9/25/13.
+//  Copyright (c) 2013 Quoc Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "POSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([POSAppDelegate class]));
+  }
+}
